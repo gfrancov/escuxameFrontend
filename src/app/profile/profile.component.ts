10a2db05043dd1caf2f79896ed77d028a30007b7
Gabriel Franco <gfranco@_HOSTNAME_>
2022-06-02 16:08:37 +0200

    Delete posts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore, addDoc, getDoc, doc, query, where, getFirestore, collection, getDocs, orderBy, limit } from '@angular/fire/firestore';
+import { Firestore, addDoc, getDoc, doc, query, where, getFirestore, collection, getDocs, deleteDoc, limit } from '@angular/fire/firestore';
+import Swal from 'sweetalert2';
 import { environment } from 'src/environments/environment';
 import { initializeApp } from "firebase/app";
 import { ActivatedRoute } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-profile',
@@ -15,12 +17,20 @@ export class ProfileComponent implements OnInit {
   userExists : boolean = false;
   userData : any;
   userPosts : any = [];
+  ownProfile : boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private firebaseAuth: AngularFireAuth) { }
 
   async ngOnInit(): Promise<void> {
    
     this.selectedUser = this.route.snapshot.params['user'];
+
+    this.firebaseAuth.authState.subscribe( authState => {
+      
+        if(this.selectedUser == authState?.displayName) {
+          this.ownProfile = true;
+        }
+      });
 
      this.getUser(this.selectedUser).then((result:any) => {
 
@@ -38,6 +48,7 @@ export class ProfileComponent implements OnInit {
       
       result.forEach((doc: any) => {
         const docResult = doc._document.data.value.mapValue.fields;
+        docResult.id = doc.id;
         this.userPosts.push(docResult);
       });
 
@@ -74,6 +85,30 @@ export class ProfileComponent implements OnInit {
 
   goToSpotify(url : string) {
     window.open(url, "popup",'width=800,height=500,');
+  }
+
+  async deletePost(postId : string) {
+    
+    const app = initializeApp(environment.firebase);
+    const db = getFirestore(app);
+
+    const postCon = doc(db, 'posts', postId);
+    await deleteDoc(postCon);
+
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Post borrado!',
+      text: 'El post seleccionado ha sido borrado!',
+      showConfirmButton: true,
+      confirmButtonColor: '#7ea966',
+      confirmButtonText: 'Ver mis posts',
+      color: '#fff',
+      background: '#181818',
+    }).then(() => {
+      window.location.href = `/u/${this.selectedUser}`;
+    });
+
   }
 
 }
